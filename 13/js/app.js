0d80d904eb79aa382aae63219a00544016065402
Lucas Maximiliano Carrizo <lucas.maxi.c@gmail.com>
2023-03-27 15:22:18 -0300

    agregando js del reto

---
+++ 13/js/app.js
@@ -0,0 +1,15 @@
+let num1 = 5
+let num2 = 4
+let resultado = 0
+resultado = num1 + num2
+console.log(resultado)
+
+resultado = num1 - num2
+console.log(resultado)
+
+resultado = num1 * num2
+console.log(resultado)
+
+resultado = num1 / num2
+console.log(resultado)
+
